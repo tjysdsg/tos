e3f518e7e7c7cb3924b160a8e5e550c63ecca7ae
tjysdsg <tjy1018543509@163.com>
2018-11-14 13:00:11 +0800

    screen

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,22 +1,5 @@
-#include "../drivers/port.h"
+#include "../drivers/screen.h"
 void main() {
-    /* Screen cursor position: ask VGA control register (0x3d4) for bytes
-     * 14 = high byte of cursor and 15 = low byte of cursor. */
-    port_byte_out(0x3d4, 14); /* Requesting byte 14: high byte of cursor pos */
-    /* Data is returned in VGA data register (0x3d5) */
-    int position = port_byte_in(0x3d5);
-    position = position << 8; /* high byte */
-
-    port_byte_out(0x3d4, 15); /* requesting low byte */
-    position += port_byte_in(0x3d5);
-
-    /* VGA 'cells' consist of the character and its control data
-     * e.g. 'white on black background', 'red text on white bg', etc */
-    
-    //move right 1 down 1, and print
-    int offset_from_vga = (position+1+80) * 2;
-
-    char *vga = 0xb8000;
-    vga[offset_from_vga] = 'X'; 
-    vga[offset_from_vga+1] = 0x0f; /* White text on black background */
+    clear_screen();
+    kprint("shit");
 }
