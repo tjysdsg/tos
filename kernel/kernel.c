ed0030ce21a1a3f41329c08a1477c4dd63db1ccd
tjysdsg <tjy1018543509@163.com>
2019-01-03 13:42:23 +0800

    reformat

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -2,15 +2,17 @@
 #include "util.h"
 
 void main() {
-	clear_screen();
-	
-	/* Fill up the screen */
-	int i = 0;
-	for (i = 0; i < 24; i++) {
-		char str[255];
-		int_to_ascii(i, str);
-		kprint_at(str, 0, i);
-	}
-	kprint_at("This text forces the kernel to scroll. Row 0 will disappear. ", 60, 24);
-	kprint_at("This text forces the kernel to scroll. Row 0 will disappear. ", 0, 24);
+    clear_screen();
+
+    /* Fill up the screen */
+    int i = 0;
+    for (i = 0; i < 24; i++) {
+        char str[255];
+        int_to_ascii(i, str);
+        kprint_at(str, 0, i);
+    }
+    kprint_at("This text forces the kernel to scroll. Row 0 will disappear. ",
+              60, 24);
+    kprint_at("This text forces the kernel to scroll. Row 0 will disappear. ",
+              0, 24);
 }
--- kernel/util.h
+++ kernel/util.h
@@ -6,4 +6,4 @@ void memory_copy(char *source, char *dest, int nbytes);
 
 char *int_to_ascii(uint32_t val, char *buf);
 
-#endif //TOS_UTIL_H
+#endif  // TOS_UTIL_H
