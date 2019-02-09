db5f4ae418fd9e0275b8181a303ec0222148cabc
tjysdsg <tjy1018543509@163.com>
2019-02-09 13:31:08 +0800

    add comments

--- boot/gdt.asm
+++ boot/gdt.asm
@@ -1,27 +1,55 @@
-gdt_start: ; don't remove the labels, they're needed to compute sizes and jumps
-    ; the GDT starts with a null 8-byte
+gdt_start:
+    ; the GDT must starts with a null 8-byte
     dd 0x0 ; 4 byte
     dd 0x0 ; 4 byte
 
 ; GDT for code segment. base = 0x00000000, length = 0xfffff
-; for flags, refer to os-dev.pdf document, page 36
-gdt_code: 
+gdt_code:
     dw 0xffff    ; segment length, bits 0-15
+
     dw 0x0       ; segment base, bits 0-15
     db 0x0       ; segment base, bits 16-23
-    db 10011010b ; flags (8 bits)
-    db 11001111b ; flags (4 bits) + segment length, bits 16-19
+
+    db 10011010b ; access byte (8 bits)
+                 ; DETAILS:
+                 ;     access bit=0;
+                 ;     readable
+                 ;     code in this segment can only be executed from the ring set in priviledge bits
+                 ;     executable bit=1 (is a code segment)
+                 ;     descriptor type=1 (either code or data segment)
+                 ;     ring 0
+                 ;     present bit=1
+
+    db 11001111b ; flags (4 bits) + the 16th to 19th bits of segment length (4 bits)
+                 ; DETAILS:
+                 ;     page granularity
+                 ;     32 bit protected mode
+
     db 0x0       ; segment base, bits 24-31
 
-; GDT for data segment. base and length identical to code segment
-; some flags changed, again, refer to os-dev.pdf
+; GDT for data segment. base = 0x00000000, length = 0xfffff
 gdt_data:
-    dw 0xffff
-    dw 0x0
-    db 0x0
-    db 10010010b
-    db 11001111b
-    db 0x0
+    dw 0xffff    ; segment length, bits 0-15
+
+    dw 0x0       ; segment base, bits 0-15
+    db 0x0       ; segment base, bits 16-23
+
+    db 10010010b ; access byte (8 bits)
+                 ; DETAILS:
+                 ;     access bit=0;
+                 ;     readable
+                 ;     data segment grows up
+                 ;     executable bit=0 (is a data segment)
+                 ;     descriptor type=1 (either code or data segment)
+                 ;     ring 0
+                 ;     present bit=1
+
+    db 11001111b ; flags (4 bits) + the 16th to 19th bits of segment length (4 bits)
+                 ; DETAILS:
+                 ;     page granularity
+                 ;     32 bit protected mode
+
+    db 0x0       ; segment base, bits 24-31
 
 gdt_end:
 
