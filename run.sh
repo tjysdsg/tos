ccf8046711ca814cb2ab7d172f0187b775e039d4
tjysdsg <tjy1018543509@163.com>
2020-04-01 10:45:20 +0800

    Set target runner, add tests

--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cargo bootimage || exit 1
-qemu-system-x86_64 -drive format=raw,file=target/x86_64-tos/debug/bootimage-tos.bin
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
 
 mod vga_buffer;
 
@@ -14,6 +16,13 @@ fn panic(info: &PanicInfo) -> ! {
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
-    panic!("BITCH!!!");
     loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
 }
