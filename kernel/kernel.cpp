c682cd7a7959b88002881b76dcd45e6cf7b80208
tjysdsg <tjy1018543509@163.com>
2022-02-05 13:14:39 +0800

    Support \n and \r

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -65,36 +65,55 @@ void terminal_initialize() {
   }
 }
 
-void terminal_setcolor(uint8_t color) {
+void terminal_set_color(uint8_t color) {
   terminal_color = color;
 }
 
-void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) {
+void terminal_put_entry_at(char c, uint8_t color, size_t x, size_t y) {
   const size_t index = y * VGA_WIDTH + x;
   terminal_buffer[index] = vga_entry(c, color);
 }
 
-void terminal_putchar(char c) {
-  terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
-  if (++terminal_column == VGA_WIDTH) {
+void auto_wrap() {
+  if (terminal_column >= VGA_WIDTH) {
     terminal_column = 0;
-    if (++terminal_row == VGA_HEIGHT)
+    if (++terminal_row >= VGA_HEIGHT)
       terminal_row = 0;
   }
 }
 
-void terminal_write(const char *data, size_t size) {
-  for (size_t i = 0; i < size; i++)
-    terminal_putchar(data[i]);
+void terminal_put_char(char c) {
+  switch (c) {
+    case '\n': {
+      ++terminal_row;
+      terminal_column = 0;
+      break;
+    }
+    case '\r': {
+      terminal_column = 0;
+      break;
+    }
+    default: {
+      terminal_put_entry_at(c, terminal_color, terminal_column, terminal_row);
+      ++terminal_column;
+      break;
+    }
+  }
+  auto_wrap();
 }
 
-void terminal_writestring(const char *data) {
+void terminal_write(const char *data, size_t size) {
+  for (size_t i = 0; i < size; i++)
+    terminal_put_char(data[i]);
+}
+
+void terminal_write_string(const char *data) {
   terminal_write(data, strlen(data));
 }
 
 extern "C" void kernel_main() {
   terminal_initialize();
 
-  /* Newline support is left as an exercise. */
-  terminal_writestring("Hello, kernel World!\n");
+  terminal_write_string("Hello, kernel World!\n");
+  terminal_write_string("Hello, kernel World!\rFuck , ");
 }
