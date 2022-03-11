8173c11974b294a0a95a422fd5230c7a67d7e1ba
tjysdsg <tjy1018543509@163.com>
2022-03-11 13:53:08 +0800

    Add fprintf stubs

---
+++ libc/stdio.cpp
@@ -0,0 +1,6 @@
+#include "stdio.h"
+
+int fprintf(FILE *stream, const char *format, ...) {
+  // TODO: implement fprintf
+  return 0;
+}
--- libc/stdio.h
+++ libc/stdio.h
@@ -1,6 +1,6 @@
 #ifndef TOS_LIBC_STDIO_H
 #define TOS_LIBC_STDIO_H
-#include <stdint.h>
+#include <stddef.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -10,10 +10,7 @@ typedef struct {
   size_t fd;
 } FILE;
 
-int fprintf(FILE *stream, const char *format, ...) {
-  // TODO: implement fprintf
-  return 0;
-}
+int fprintf(FILE *stream, const char *format, ...);
 
 extern FILE *stdin;
 extern FILE *stdout;
