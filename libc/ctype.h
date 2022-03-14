bec80c98e2e72d063eb46bc941d95b353e65b021
tjysdsg <tjy1018543509@163.com>
2022-03-15 01:06:04 +0800

    Fix broken ctype.h

--- libc/ctype.h
+++ libc/ctype.h
@@ -5,24 +5,20 @@
 extern "C" {
 #endif
 
-#define _ASCIIRANGE(c, start, len) (((c) - (start)) < (len))
-static inline int islower(int c)  { return _ASCIIRANGE(c, 0x61, 26); }
-static inline int isupper(int c)  { return _ASCIIRANGE(c, 0x41, 26); }
-static inline int isdigit(int c)  { return _ASCIIRANGE(c, 0x30, 10); }
-static inline int isbdigit(int c) { return _ASCIIRANGE(c, 0x30,  2); }
-static inline int isodigit(int c) { return _ASCIIRANGE(c, 0x30,  8); }
-static inline int isxdigit(int c) { return _ASCIIRANGE(c | 32, 0x61, 6) || isdigit(c); }
-static inline int isalpha(int c)  { return _ASCIIRANGE(c | 32, 0x61, 26); }
-static inline int isprint(int c)  { return _ASCIIRANGE(c, 0x20, 95) || _ASCIIRANGE(c, 128, 127); }
-static inline int isgraph(int c)  { return _ASCIIRANGE(c, 0x21, 94); }
-static inline int isspace(int c)  { return _ASCIIRANGE(c, 0x9, 5) || c == 0x20; }
-static inline int isalnum(int c)  { return isalpha(c) || isdigit(c); }
-static inline int ispunct(int c)  { return !isalnum(c) && isgraph(c); }
-static inline int isblank(int c)  { return c == 0x20 || c ==  0x9; }
-static inline int iscntrl(int c)  { return c  < 0x20 || c == 0x7f; }
-static inline int tolower(int c) { return isupper(c) ? c + 0x20 : c; }
-static inline int toupper(int c) { return islower(c) ? c - 0x20 : c; }
-#undef _ASCIIRANGE
+// https://github.com/bminor/musl/blob/master/src/ctype
+
+static inline int islower(int c) { return (unsigned) c - 'a' < 26; }
+static inline int isupper(int c) { return (unsigned) c - 'A' < 26; }
+static inline int isdigit(int c) { return (unsigned) c - '0' < 10; }
+static inline int isxdigit(int c) { return isdigit(c) || ((unsigned) c | 32) - 'a' < 6; }
+static inline int isalpha(int c) { return ((unsigned) c | 32) - 'a' < 26; }
+static inline int isprint(int c) { return (unsigned) c - 0x20 < 0x5f; }
+static inline int isgraph(int c) { return (unsigned) c - 0x21 < 0x5e; }
+static inline int isspace(int c) { return c == ' ' || (unsigned) c - '\t' < 5; }
+static inline int isalnum(int c) { return isalpha(c) || isdigit(c); }
+static inline int ispunct(int c) { return !isalnum(c) && isgraph(c); }
+static inline int tolower(int c) { return isupper(c) ? c | 32 : c; }
+static inline int toupper(int c) { return islower(c) ? c & 0x5f : c; }
 
 #ifdef __cplusplus
 }
